8c73c572e34228f01475706f487a614165e22d5f
David Hernández <davidalfredoh146@gmail.com>
2023-08-16 16:57:48 -0600

    Make the app prettier
    Extract Widget for pair word.
    Padding the widget.
    Theme and text theme.
    Center the UI.

--- lib/main.dart
+++ lib/main.dart
@@ -39,20 +39,62 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    // Should no longer refers to the whole appState
+    var pair = appState.current; 
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+      // It only taked up as much horizontal space as its children need
+      // Center the column itself
+      body: Center(
+        child: Column(
+          // Centers the children inside the Column along its main (vertical) axis
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+      
+            // Takes space and doesn't render anything
+            SizedBox(height: 10),
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    // app's current theme
+    final theme = Theme.of(context); 
+    // access the app's font theme
+    // ! bang operator
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary, 
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
